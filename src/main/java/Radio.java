ead66cb0a0b1da4de721d962236cd54b01488e2d
Grigoriy Kaygorodtsev <grigoriy.kaygorotsev@gmail.com>
2023-12-13 19:55:57 +0300

    New commit

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -2,6 +2,19 @@ public class Radio {
     private int currentStation;
 
     private int currentVolume;
+    private int maxStation;
+
+
+    public Radio () {
+        this.maxStation = 9;
+
+    }
+
+    public Radio(int stationsCount) {
+        this.maxStation = stationsCount - 1;
+
+    }
+
 
     public int getCurrentStation() {
         return currentStation;
@@ -11,14 +24,14 @@ public class Radio {
         if (currentStation < 0) {
             return;
         }
-        if (currentStation > 9) {
+        if (currentStation > maxStation) {
             return;
         }
         this.currentStation = currentStation;
     }
 
     public void next() {
-        if (currentStation != 9) {
+        if (currentStation != maxStation) {
             currentStation++;
         } else {
             currentStation = 0;
@@ -29,7 +42,7 @@ public class Radio {
         if (currentStation != 0) {
             currentStation--;
         } else {
-            currentStation = 9;
+            currentStation = maxStation;
         }
     }
 
